40cfdb7c1c15fa9caaccca1ed1f433bb1e7102de
xg4 <xingor4@gmail.com>
2022-06-10 13:36:10 +0800

    chore: public houses api

--- src/house/house.controller.ts
+++ src/house/house.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common'
+import { Public } from '../auth/decorator'
 import { HouseService } from './house.service'
 
 @Controller()
 export class HouseController {
   constructor(private readonly houseService: HouseService) {}
 
+  @Public()
   @Get('houses')
   getAll() {
     return this.houseService.getAll()
--- src/house/house.service.ts
+++ src/house/house.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common'
+import { SHA256 } from 'crypto-js'
 import { PrismaService } from '../prisma/prisma.service'
 
 @Injectable()
 export class HouseService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async getAll() {
-    return this.prisma.house.findMany()
+  async getAll(hash?: string) {
+    const houses = await this.prisma.house.findMany()
+    // if (hash) {
+    //   const newHash = SHA256(houses.toString()).toString()
+    //   if (newHash === hash) {
+    //     return
+    //   }
+    // }
+    return houses
   }
 }
