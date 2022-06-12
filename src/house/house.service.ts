e7b910f3454ed9ef4ecc25eef43d541d7a6759f5
xg4 <xingor4@gmail.com>
2022-06-12 10:03:45 +0800

    chore: update deps

--- src/house/house.service.ts
+++ src/house/house.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common'
-import { SHA256 } from 'crypto-js'
 import { PrismaService } from '../prisma/prisma.service'
 
 @Injectable()
@@ -8,12 +7,6 @@ export class HouseService {
 
   async getAll(hash?: string) {
     const houses = await this.prisma.house.findMany()
-    // if (hash) {
-    //   const newHash = SHA256(houses.toString()).toString()
-    //   if (newHash === hash) {
-    //     return
-    //   }
-    // }
     return houses
   }
 }
